cbeb504c45ff556a5de54e7a08de0b013503457b
mdmartinvega <mdmartinvega@gmail.com>
2021-10-19 11:42:08 +0200

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+function GifExpertApp() {
+
+    const [categories, setCategories ] = useState(['Dragon Ball'])
+
+ /*    const handleAdd = () => {
+       return setCategories([...categories, 'Kamehameha']);
+    } */
+
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr />
+            <ol>
+                {
+                    categories.map(category => 
+                    <GifGrid 
+                        key={category}
+                        category={category}/> )
+                }
+            </ol>
+        </div>
+       
+    )
+}
+
+
+export default GifExpertApp
+
