5c2224b74c574b5952745b1f541d61105fd42bcb
mdmartinvega <mdmartinvega@gmail.com>
2021-10-19 13:22:49 +0200

    Tests en getGifs

---
+++ src/tests/helpers/getGifs.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import { getGifs } from "../../helpers/getGifs";
+
+describe('Pruebas con getGifs Fetch', () => {
+
+    
+    test('Debe de traer 10 elementos', async() => {
+
+        const gifs = await getGifs('One Punch');
+
+        expect(gifs.length).toBe(10);
+    })    
+
+    test('Debe de traer 10 elementos 2', async() => {
+
+        const gifs = await getGifs('');
+
+        expect(gifs.length).toBe(0);
+    })  
+
+ 
+
+})
