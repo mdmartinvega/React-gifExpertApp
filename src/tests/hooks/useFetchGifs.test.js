5f1b7f1b346780f511d87962675d50186261e109
mdmartinvega <mdmartinvega@gmail.com>
2021-10-21 11:23:25 +0200

    Más pruebas con useFetch

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -7,16 +7,27 @@ import { renderHook } from '@testing-library/react-hooks';
 describe('Pruebas en UsefectGifs.js', () => {
     
 
-    test('Debe retornar el estado inicial', () => {
+    test('Debe retornar el estado inicial', async() => {
 
-        const {result} = renderHook( () => useFetchGifs('One Punch'));
+        const {result, waitForNextUpdate} = renderHook( () => useFetchGifs('One Punch'));
         const {data, loading} = result.current;
 
         //const { data, loading} = useFetchGifs('One Punch');
         console.log(data, loading);
-
+        await waitForNextUpdate();
         expect( data ).toEqual([]);
         expect(loading).toBe(true);
+
+    })
+
+    test('Debe retornar un arreglo de imgs y el loading en false', async() => {
+
+        const {result, waitForNextUpdate} = renderHook( () => useFetchGifs('One Punch'));
+        await waitForNextUpdate();
+        const {data, loading} = result.current;
+
+        expect( data.length ).toBe(10);
+        expect(loading).toBe(false);
 
     })
 
